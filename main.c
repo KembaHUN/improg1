3013a5c62aa7a4ca1c10f31d0fd4d848f52e63e8
KembaHUN <113339420+KembaHUN@users.noreply.github.com>
2022-09-12 10:23:59 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n = 42; // integer
+    unsigned int un = 10; // positive
+    float f = 3.14;
+    double d = 3.141592; // more digits
+    char c = 'a'; // character
+    
+    printf("%d %u %f %f %c\n", n, un, f, d, c);
+}
